697cabcc14209419277cfd4915fe3128ebb514c7
Desmondonam <desmondonam@gmail.com>
2025-06-05 19:10:40 -0400

    add STATICFILES_STORAGE

--- classroom_manager/settings.py
+++ classroom_manager/settings.py
@@ -103,7 +103,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Enable WhiteNoise for static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
